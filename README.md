dae144005d466d4de92163b37f71de893f58d353
splendidsummer <417846472@qq.com>
2022-10-02 16:29:51 +0200

    update data exploration

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # dl_project1
 Deep learning CNN Assignment
+
+## How to design a CNN network
+
+## Incomplete Hyperparameter List 
+
+
+### Incomplete Hyperparameter list
+* Batch size 
+* Number of epochs 
+* Learning Rate 
+* Weight decay
+* Activation function 
+* Input normalization 
+* Weight initialization 
+* Data augmentation 
+* Network Capacity(layers and neurons) 
+* Early stopping policy 
+* Normalization layer + hyperparameters
+* Loss function
+#### Convolution
+kernel size, stride, padding, dilated rate
+#### Pooling
+kernel size, stride, padding, avg/max/global average
+#### Dilated convolution
+Set in the normal convolution 
+#### Skip Connection 
+#### Inception block 
+#### CBR or CRB Block 
+### CCC Block 
+
+
+
---
+++ utils.py
@@ -0,0 +1,41 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import pickle, json
+
+data_columns = ['Image file', 'Medium', 'Museum', 'Museum-based instance ID', 'Subset',
+                'Width', 'Height', 'Product size', 'Aspect ratio']
+
+data_classes = ['Oil on canvas', 'Graphite', 'Glass', 'Limestone', 'Bronze',
+                'Ceramic', 'Polychromed wood', 'Faience', 'Wood', 'Gold', 'Marble',
+                'Ivory', 'Silver', 'Etching', 'Iron', 'Engraving', 'Steel',
+                'Woodblock', 'Silk and metal thread', 'Lithograph',
+                'Woven fabric ', 'Porcelain', 'Pen and brown ink', 'Woodcut',
+                'Wood engraving', 'Hand-colored engraving', 'Clay',
+                'Hand-colored etching', 'Albumen photograph']
+
+
+def read_split_data(csv_data_file, csv_label_file):
+    """
+    :param csv_file: csv file in MAMe_metadata folder:
+        1. MAME_dataset6
+        2. MAME_labels
+    :return:
+    Organized file path in dict format:
+        dict key: image class
+        dict value: all the image data paths corresponding to the key
+    save the processed data into pkl file as well
+    """
+    data = pd.read_csv(csv_data_file)
+    labels = pd.read_csv(csv_label_file)
+
+    return None
+
+
+def read_csv():
+    pass
+
+
+def explore_data():
+    pass
+
